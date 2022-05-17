dfede61156baeca4f21cd97c6d11b7011ec7b124
Katsu1001 <k.katsumi19991001@gmail.com>
2022-05-17 17:51:00 +0900

    マイグレーション

--- db/migrate/20220517072206_create_images.rb
+++ db/migrate/20220517072206_create_images.rb
@@ -1,7 +1,8 @@
 class CreateImages < ActiveRecord::Migration[6.0]
   def change
     create_table :images do |t|
-
+      t.references :user,  null: false, foreign_key: true
+      t.string :user_image, null: false
       t.timestamps
     end
   end
--- db/migrate/20220517072510_create_user_bodies.rb
+++ db/migrate/20220517072510_create_user_bodies.rb
@@ -1,7 +1,11 @@
 class CreateUserBodies < ActiveRecord::Migration[6.0]
   def change
     create_table :user_bodies do |t|
-
+      t.references :user,  null: false, foreign_key: true
+      t.integer :weight
+      t.integer :height
+      t.string :gender
+      t.integer :birth_year
       t.timestamps
     end
   end
